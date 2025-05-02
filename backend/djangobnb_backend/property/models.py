a5c6d07f45cc4b90e5ce7180e163327e5fb5dbda
urutear <keitadance0208@gmail.com>
2025-05-02 22:08:55 +0900

    create Reservation model at backend

--- backend/djangobnb_backend/property/models.py
+++ backend/djangobnb_backend/property/models.py
@@ -2,6 +2,7 @@ import uuid
 
 from django.conf import settings
 from django.db import models
+from django.core.exceptions import ValidationError
 
 from useraccount.models import User
 
@@ -23,3 +24,29 @@ class Property(models.Model):
 
   def image_url(self):
     return f'{settings.WEBSITE_URL}{self.image.url}'
+  
+class Reservation(models.Model):
+  id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+  property_obj = models.ForeignKey(Property, related_name="reservations", on_delete=models.CASCADE) # Property.reservations.all() で反対側から参照可能
+  checkin_date = models.DateField()
+  checkout_date = models.DateField()
+  guests = models.IntegerField(default=1)
+  booked_by = models.ForeignKey(User, related_name='reservations', on_delete=models.CASCADE)
+  booked_at = models.DateTimeField(auto_now_add=True)
+  
+  @property
+  def number_of_nights(self):
+    return (self.checkout_date - self.checkin_date).days
+  
+  @property
+  def total_price(self):
+    return self.property.price_per_night * self.number_of_nights
+  
+  # override full_clean() that is called whenever a model instance is saved
+  def clean(self):
+    if self.checkout_date <= self.checkin_date:
+      raise ValidationError("check-out date must be after check-in date")
+    
+  def save(self, *args, **kwargs):
+    self.full_clean()
+    super().save(*args, **kwargs)
